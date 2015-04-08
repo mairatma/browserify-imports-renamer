77217dd7b200faec1c0bc5e9f90bc600a52676b9
Maira Bello <maira.araujo@liferay.com>
2015-04-08 11:12:43 -0300

    Adds initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+browserify-imports-renamer
+===================================
+
+A browserify transform for renaming ES6 import declarations.
---
+++ index.js
@@ -0,0 +1,33 @@
+var merge = require('merge');
+var recast = require('recast');
+var renamer = require('es6-imports-renamer');
+var through = require('through');
+
+module.exports = function(options) {
+	options = options || {};
+
+	function transform(filename) {
+		var data = '';
+		function write(buf) {
+			data += buf;
+		}
+
+		return through(write, function() {
+			var self = this;
+	        var ast = recast.parse(data);
+	        var sources = [ { ast: ast, path: filename } ];
+	        var renamerOptions = merge({}, options, {sources: sources});
+	        renamer(renamerOptions, function(results) {
+	        	try {
+					var contents = recast.print(results[0].ast);
+		            self.queue(contents.code);
+				} catch (error) {
+					self.emit('error', error);
+				}
+				self.queue(null);
+			});
+	    });
+	}
+
+	return transform;
+}
